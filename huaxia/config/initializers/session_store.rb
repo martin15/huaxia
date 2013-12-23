c14a84531ae6655ea418839b0ab100d71bad5505
root <root@test.virtualizor.com>
2013-12-23 13:00:08 +0000

    fixing code

--- huaxia/config/initializers/session_store.rb
+++ huaxia/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Huaxia::Application.config.session_store :cookie_store, key: '_huaxia_session'
+Huaxia::Application.config.session_store :cookie_store, :key => '_huaxia_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
