eb2770d4878788a9a06943fe4836de92a17b5061
martin <martin.me15@yahoo.com>
2014-12-22 14:54:35 +0800

    handling invalid params page

--- huaxia/app/controllers/application_controller.rb
+++ huaxia/app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :reload_data
+  before_filter :params_validation
+
+  def params_validation
+    unless params[:page].to_s != "0" && (params[:page].nil? || /^\d+$/ =~ params[:page])
+      respond_to do |format|
+        format.html { render :file => "#{Rails.root}/public/404",
+                             :layout => false, :status => :not_found }
+      end
+    end
+  end
 
   def reload_data
     @informations = Information.showed_informations
