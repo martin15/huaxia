f6e71443704b07c303eff99a65660727c4b51f5b
Martin <martin.me15@yahoo.com>
2016-03-16 08:36:08 +0700

    add rturn to break if object not found

--- huaxia/app/controllers/features_controller.rb
+++ huaxia/app/controllers/features_controller.rb
@@ -5,6 +5,7 @@ class FeaturesController < ApplicationController
     @program_category = FeatureCategory.find_by_permalink(params[:category_permalink])
     if @program_category.nil? || params[:category_permalink].nil?
       redirect_to programs_path(@program_categories.first.permalink)
+      return
     end
     @programs = @program_category.features
     puts @programs.inspect
--- huaxia/app/controllers/student_associations_controller.rb
+++ huaxia/app/controllers/student_associations_controller.rb
@@ -5,7 +5,7 @@ class StudentAssociationsController < ApplicationController
     @student_association = StudentAssociation.find_by_permalink(params[:permalink])
     if @student_association.nil?
       redirect_to student_associations_path(StudentAssociation.default.permalink)
+      return
     end
-
   end
 end
